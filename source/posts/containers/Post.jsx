99c7b17b11beb7b848b46ad984ccecbc5d37f262
gllorente <llorentegerman@gmail.com>
2017-01-16 10:05:53 -0300

    initial commit

---
+++ source/posts/containers/Post.jsx
@@ -0,0 +1,72 @@
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
+
+import api from '../../api.js';
+
+import styles from './Post.css';
+
+class Post extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            user: props.user || null,
+            comments: props.comments || null,
+        }
+    }
+
+    async componentDidMount() {
+        if (!!this.state.user && !!this.state.comments) return this.setState({ loading: false });
+
+        const [
+            user,
+            comments,
+        ] = await Promise.all([
+            !this.state.user ? api.users.getSingle(this.props.userId) : Promise.resolve(this.state.user),
+            !this.state.comments ? api.posts.getComments(this.props.id) : Promise.resolve(this.state.comments),
+        ]);
+
+        this.setState({
+            loading: false,
+            user,
+            comments,
+        });
+    }
+
+    render() {
+        return (
+            <article id={`post-${this.props.id}`} className={styles.post}>
+
+                <h2 className={styles.title} >
+                    <Link to={`/post/${this.props.id}`} >
+                        {this.props.title}
+                    </Link>
+                </h2>
+                <p className={styles.body}>
+                    {this.props.body}
+                </p>
+                {!this.state.loading && (
+                    <div className={styles.meta}>
+                        <Link to={`/user/${this.state.user.id}`} className={styles.user}>
+                            {this.state.user.name}
+                        </Link>
+
+                        <span className={styles.comments}>
+                            Hay {this.state.comments.length} comentarios
+                        </span>
+                    </div>
+                )}
+            </article>
+        )
+    }
+}
+
+Post.propTypes = {
+    id: PropTypes.number,
+    userId: PropTypes.number,
+    title: PropTypes.string,
+    body: PropTypes.string,
+};
+
+export default Post;
---
+++ webpack/webpack.server.config.js
@@ -0,0 +1,33 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: './source/server.js',
+    output: {
+        filename: 'index.js',
+        path: './built/server',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.json$/,
+                loader: 'json',
+            },
+            {
+                test: /\.jsx?$/,
+                loader: 'babel',
+                exclude: /(node_modules)/,
+                query: {
+                    presets: ['latest-minimal', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style', 'css?modules')
+            },
+        ],
+    },
+    target: 'node',
+    plugins: [
+        new ExtractTextPlugin('../statics/styles.css'),
+    ],
+};
